47da9ee3b07b6159b81cc12ff44a983f163b8328
PsyHow <viktorburnyshev@gmail.com>
2021-10-12 12:09:56 +0600

    add style for Users Page

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -10,57 +10,33 @@ export const Users = (props: UsersPropsType) => {
     if (props.usersPage.users.length === 0) {
         axios.get<UsersType[]>("https://social-network.samuraijs.com/api/1.0/users")
             .then((response) => {
-                debugger
                 // @ts-ignore
                 props.setUsers(response.data.items)
             })
-       /* props.setUsers([
-            {
-                id: v1(),
-                avatar: 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/ce54bf11889067.562541ef7cde4.png',
-                followed: false,
-                fullName: 'Viktor',
-                status: 'Like a boss',
-                location: {city: 'Pavlodar', country: 'Kazakhstan'}
-            },
-            {
-                id: v1(),
-                avatar: 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/ce54bf11889067.562541ef7cde4.png',
-                followed: false,
-                fullName: 'Ilyas',
-                status: 'Ilysimus',
-                location: {city: 'Almaty', country: 'Kazakhstan'}
-            },
-            {
-                id: v1(),
-                avatar: 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/ce54bf11889067.562541ef7cde4.png',
-                followed: true,
-                fullName: 'Diyar',
-                status: 'I wont to be a HackerMan',
-                location: {city: 'Pavlodar', country: 'Kazakhstan'}
-            }
-        ])*/
     }
 
     return (
         <div className={s.wrapper}>
-            {props.usersPage.users.map(u => <div key={u.id}>
-                <span>
+            {props.usersPage.users.map(u => <div className={s.itemsWrapper} key={u.id}>
+                <div className={s.items}>
                     <div>
                         <img src={u.photos.small !== null ? u.photos.small : userPhoto}/>
                     </div>
-                    <div>
+                    <br/>
+                    <div className={s.button}>
                         {u.followed
                             ? <button onClick={() => props.unFollow(u.id)}>UnFollow</button>
                             : <button onClick={() => props.follow(u.id)}>Follow</button>
                         }
                     </div>
-                </span>
-                <span>
-                    <span>
+                    <br/>
+                    <div className={s.userInfo}>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
-                    </span>
+                    </div>
+
+                </div>
+                <span>
                     <span>
                         <div>{u.location?.country}</div>
                         <div>{u.location?.city}</div>
